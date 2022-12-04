6834c8e0dafc96ce2eb306b60415aeac06a42029
Sayberix <mr.maaks@gmail.com>
2022-12-04 21:38:55 +0700

    задание выполнено

--- home_work_2.4.py
+++ home_work_2.4.py
@@ -9,3 +9,12 @@
 #    Ввод: 0.001
 #    Вывод: 3.141
 
+import math as m
+
+def toFixed(number: float, digits: int) -> str:
+    return f"{number:.{digits}f}"
+
+accuracy = input('Введите точность через знак разделитель "." (например - "0.001"):')
+discharge = len(str(accuracy).split('.')[1])
+pi = 2 * (m.asin(m.sqrt(1 - 1 ** 2)) + m.fabs(m.asin(1)))
+print('число PI с заданной точностью:',toFixed(pi,discharge))
