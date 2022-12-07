841d207c72e2d7edf9b192f3419bc9ea9a5b1924
Sayberix <mr.maaks@gmail.com>
2022-12-07 23:39:21 +0700

    задание выполнено. Не разобрался с циклом for. Реализовал через цикл while

--- home_work_2.4.py
+++ home_work_2.4.py
@@ -4,16 +4,30 @@
 # Ввод: [1, 1, 2, 3, 4, 4, 4]
 # Вывод: [2, 3]
 
-def find_repeating_elements(number: int, list_unique_elements: list) -> bool:
-    for i in range(len(list_unique_elements)):
-        if number == list_unique_elements[i] and i > 0:
-            list_resault.pop(list_unique_elements[i])
-            return False, list_unique_elements
-    return True
 
-list_number = [1, 1, 2, 3, 4, 4, 4, 2, 3]
-list_resault = []
-for i in range(1, len(list_number) - 1):
-    if list_number[i] != list_number[i - 1] and list_number[i] != list_number[i + 1] and find_repeating_elements(list_number[i],list_resault):
-        list_resault.append(list_number[i])
-print('Список неповторяющихся элементов:',list_resault)
+def find_repeating_elements(list_number: list) -> dict:
+    dict_elements = {}
+    for i in range(len(list_number)):
+        count = 1
+        for k in range(len(list_number)):
+            if list_number[i] == list_number[k] and i is not k:
+                count += 1
+        dict_elements[i] = count
+    return dict_elements
+
+def deleting_elements(my_list: list, my_dict: dict) -> list:
+    #for i in range(len(my_list) - 1,0,-1):
+    #    ...
+    #    if my_dict[i] > 1:
+    #        my_list.pop(i)
+    i = len(my_list) - 1
+    while i >=0:
+        if my_dict[i] > 1:
+            my_list.pop(i)
+        i -= 1
+    return my_list
+
+
+list_number = [1, 1, 2, 3, 4, 4, 4]
+
+print('Список неповторяющихся элементов:',deleting_elements(list_number, find_repeating_elements(list_number)))
