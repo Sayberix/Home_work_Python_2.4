153a1cf38a3d1f06c47a8e62bdd1a18d11b6228f
Sayberix <mr.maaks@gmail.com>
2022-12-09 00:43:50 +0700

    реализована печать вычисленого многочлена на экран

--- home_work_2.4.py
+++ home_work_2.4.py
@@ -24,15 +24,47 @@ def recive_text_file (name_file: str) -> str:
         polynomial_str = polynomial.read().split()
     return polynomial_str
 
-#coef_pars = []
-#for i in range(len(polynomial_1_str)):
-#    if polynomial_1_str[i][0].isdigit() and int(polynomial_1_str[i][0]) != 0:
-#        coef_pars.append(int(polynomial_1_str[i][0]))
-#return coef_pars
+def recive_coef (polynomial_str: str) -> list:
+    list_coef = []
+    for i in range(len(polynomial_str)):
+        if polynomial_str[i][0].isdigit() and int(polynomial_str[i][0]) != 0:
+            list_coef.append(int(polynomial_str[i][0]))
+    return list_coef
 
+def sum_coef(list_coef_1: list, list_coef_2: list) -> list:
+    sum_coef_list = []
+    #try:
+    if len(list_coef_1) == len(list_coef_2):
+         for i in range(len(list_coef_1)):
+             sum_coef_list.append(list_coef_1[i] + list_coef_2[i])
+        #else:
+        #    raise Exception("Многочлены не одинаковой степени! Программа завершает работу!")
+    return sum_coef_list
+
+def polynomial_formation(sum_coef_list: list) -> str:
+    degree_list = ['', '', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹']
+    list_polynomial = ''
+    for i in range(len(sum_coef_list) - 1,-1,-1):
+        if i != 0:
+            list_polynomial += str(sum_coef_list[len(sum_coef_list) - 1 - i]) + '*x' + degree_list[i] + ' + '
+        else:
+            list_polynomial += str(sum_coef_list[len(sum_coef_list) - 1 - i]) + ' = 0'
+    return list_polynomial
+
+
+# Получаем сырые списки строк многочленов
 polynomial_str_1 = recive_text_file('polynomial_1')
 polynomial_str_2 = recive_text_file('polynomial_2')
-...
+
+# Получаем списки кооэфициентов многочленов
+list_coef_1 = recive_coef(polynomial_str_1)
+list_coef_2 = recive_coef(polynomial_str_2)
+
+# Перемножаем кооэфициенты многочленов и выводим
+print(polynomial_formation(sum_coef(list_coef_1, list_coef_2)))
+
+
+
 
 #for i in range(len(list_number)):
 #    list_number[i] = int(list_number[i])
